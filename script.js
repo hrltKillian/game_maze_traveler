8397b1e8eb97276e7a99f22710cb1ef17876f3eb
Killian Herlant <killian.herlant@gmail.com>
2024-05-15 16:00:12 +0200

    added size selector 1<=x<=40

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let grid = document.querySelector("#grid");
+let selectorSize = document.getElementById("selectorSize");
+let generateButton = document.getElementById("generate");
 
-let size = 64;
+generateButton.addEventListener("click", () => {
+    if (selectorSize.value < 1 || selectorSize.value > 40) {
+        alert("Please enter a number between 1 and 40.");
+        return;
+    }
+    generateGrid(selectorSize.value);
+});
 
-grid.style.setProperty("--n", size);
-
-for (let i = 0; i < size * size; i++) {
-  const div = document.createElement("div");
-  div.classList.add("cell");
-  grid.appendChild(div);
+function generateGrid(size) {
+    grid.innerHTML = "";
+    grid.style.setProperty("--n", size);
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.classList.add("cell");
+        grid.appendChild(div);
+    }
 }
